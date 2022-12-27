58edd4f551ce983280a4c3c88c3f221bae574c97
Jymy Escobar <jdany.escobar@gmail.com>
2022-12-26 22:11:14 -0600

    Fin sección 12 - version 5.0.0

--- curso-node-restserver-4.0.0/models/server.js
+++ curso-node-restserver-4.0.0/models/server.js
@@ -57,7 +57,7 @@ class Server {
 
     listen() {
         this.app.listen( this.port, () => {
-            console.log('Servidor corriendo en http://localhost:' + this.port );
+            console.log('Servidor corriendo en puerto', this.port );
         });
     }
 
